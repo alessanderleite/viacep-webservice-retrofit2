ad4b52f5e8e5ba62cd0c26c718f0b9bc66a0a248
alessanderleite <alessander.logistica@gmail.com>
2019-05-31 10:09:03 -0300

    add the @GET() annotation

--- app/src/main/java/br/com/alessanderleite/viacepwebservice/service/APIRetrofitService.java
+++ app/src/main/java/br/com/alessanderleite/viacepwebservice/service/APIRetrofitService.java
@@ -1,4 +1,25 @@
 package br.com.alessanderleite.viacepwebservice.service;
 
+import java.util.List;
+
+import br.com.alessanderleite.viacepwebservice.model.CEP;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
 public interface APIRetrofitService {
+
+    String BASE_URL = "https://viacep.com.br/ws/";
+
+    /* Return the object CEP list */
+    @GET("{estado}/{cidade}/{endereco}/json/")
+    Call<List<CEP>> getCEPByCidadeEstadoEndereco(
+            @Path("estado") String estado,
+            @Path("cidade") String cidade,
+            @Path("endereco") String endereco
+    );
+
+    /* Return only an object CEP */
+    @GET("{CEP}/json/")
+    Call<CEP> getEnderecoByCEP(@Path("CEP") String CEP);
 }
